b34347a9d1383cd6c728b1756cb2ff4e59760dfb
Nikola <nikola.1996petrovic@yahoo.com>
2023-03-06 22:20:10 +0100

    Finished basic layout of view

--- ToDoList/View/ToDoList.xaml.cs
+++ ToDoList/View/ToDoList.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ToDoList
+namespace ToDoList.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ToDoList.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ToDoList : Window
     {
-        public MainWindow()
+        public ToDoList()
         {
             InitializeComponent();
-
         }
     }
 }
---
+++ ToDoList/ViewModel/ToDoVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Model;
+
+namespace ToDoList.ViewModel
+{
+    public class ToDoVM : INotifyPropertyChanged
+    {
+        public ObservableCollection<ToDoItem>? ToDoItems { get; set; }
+        public ToDoVM() 
+        {
+            ToDoItems = new ObservableCollection<ToDoItem>();
+            //if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            //{
+                ToDoItems.Add(new ToDoItem { description = "Test4", isDone = false, id = 1 });
+                ToDoItems.Add(new ToDoItem { description = "Test2", isDone = false, id = 1 });
+                ToDoItems.Add(new ToDoItem { description = "Test3", isDone = false, id = 1 });
+           // }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
