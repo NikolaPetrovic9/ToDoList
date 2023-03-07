e696aa0a90152f41a07993f60dd8c5f28240dd2c
Nikola Petrovic <nikola.petrovic@elplant.com>
2023-03-07 16:46:18 +0100

    Added commands and finished styling

---
+++ ToDoList/ViewModel/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ToDoList.Model;
+
+namespace ToDoList.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        public event EventHandler? CanExecuteChanged;
+        
+        public RelayCommand(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+    }
+}
--- ToDoList/ViewModel/ToDoVM.cs
+++ ToDoList/ViewModel/ToDoVM.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using ToDoList.Model;
 
 namespace ToDoList.ViewModel
@@ -12,17 +13,65 @@ namespace ToDoList.ViewModel
     public class ToDoVM : INotifyPropertyChanged
     {
         public ObservableCollection<ToDoItem>? ToDoItems { get; set; }
-        public ToDoVM() 
+        private string newtoDoItem;
+
+        public ICommand AddItemCommand { get; }
+        public ICommand DelleteAllItemsCommand { get; }
+        public ICommand DelleteAllDoneItemsCommand { get; }
+
+        public string NewToDoItem
+        {
+            get
+            { return newtoDoItem; }
+            set
+            {
+                newtoDoItem = value;
+                OnPropertyChanged(nameof(newtoDoItem));
+            }
+        }
+
+        public ToDoVM()
         {
             ToDoItems = new ObservableCollection<ToDoItem>();
-            //if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
-            //{
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
                 ToDoItems.Add(new ToDoItem { description = "Test4", isDone = false, id = 1 });
                 ToDoItems.Add(new ToDoItem { description = "Test2", isDone = false, id = 1 });
                 ToDoItems.Add(new ToDoItem { description = "Test3", isDone = false, id = 1 });
-           // }
+            }
+            AddItemCommand = new RelayCommand(addNewItemToList);
+            DelleteAllItemsCommand = new RelayCommand(deleteAllItems);
+            DelleteAllDoneItemsCommand = new RelayCommand(deleteAllDoneItems);
         }
 
+        public void addNewItemToList()
+        {
+            if(ToDoItems != null && !String.IsNullOrWhiteSpace(newtoDoItem))
+            {
+                ToDoItems.Add(new ToDoItem { description = newtoDoItem, isDone = false, id = 1 });
+                NewToDoItem = String.Empty;
+            }
+        }
+        public void deleteAllItems()
+        {
+            if (ToDoItems != null)
+            {
+                ToDoItems.Clear();
+            }
+        }
+        public void deleteAllDoneItems()
+        {
+            if (ToDoItems != null)
+            {
+                foreach (var item in ToDoItems.ToList())
+                {
+                    if (item.isDone)
+                    {
+                        ToDoItems.Remove(item);
+                    }
+                }
+            }
+        }
         public event PropertyChangedEventHandler? PropertyChanged;
 
         private void OnPropertyChanged(string propertyName)
